1f8b59ee74bbd8015f1eb9eab5a924877629b68a
estimatt <112658431+estimatt@users.noreply.github.com>
2022-10-21 10:39:21 -0400

    Exercise 3 done.

--- w0d1/w0d1mcs.py
+++ w0d1/w0d1mcs.py
@@ -8,6 +8,7 @@ from fancy_einsum import einsum
 
 import utils 
 
+### Exercise 1: DFT
 def DFT_1d_oneway(arr: np.ndarray) -> np.ndarray:
     """ Returns the DFT of the array `arr`, using the equations given. """
     N = arr.__len__()
@@ -18,6 +19,8 @@ def DFT_1d_oneway(arr: np.ndarray) -> np.ndarray:
     left = np.exp(left)
 
     return left @ arr
+
+### Exercise 2: Inverse DFT
 def DFT_1d(arr: np.ndarray, inverse=False) -> np.ndarray:
     """ Returns the DFT or IDFT of the array `arr`. """
     N = arr.__len__()
@@ -37,7 +40,17 @@ def DFT_1d(arr: np.ndarray, inverse=False) -> np.ndarray:
 utils.test_DFT_func(DFT_1d)
 
 
+### Exercise 3: DFT test function using known results
+
+def DFT_test():
+    x = np.array([1+0j, 2-1j, 0-1j, -1+2j])
+    y = np.array([2+0j, -2-2j, 0-2j, 4+4j])
+
+    np.testing.assert_allclose(y, DFT_1d(x))
+    np.testing.assert_allclose(x, DFT_1d(y,inverse=True))
+
+DFT_test()
+
 # %%
 
 # Testing
-
