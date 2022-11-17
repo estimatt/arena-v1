20979cf2313a0068a259b25c7b004bba00b54b73
estimatt <112658431+estimatt@users.noreply.github.com>
2022-11-17 11:52:11 -0700

    Final update

--- w1/w1d2.py
+++ w1/w1d2.py
@@ -35,7 +35,7 @@ class PositionalEncoding(nn.Module):
         x shape is (batch, seq_len, embedding_dim)
         """
         seq_len = x.shape[-2]
-        return x + self.P[:seq_len, :]
+        return x + self.P[:seq_len, :]   # TODO: Understand how this works.
 
 
 # %%
@@ -51,25 +51,16 @@ px.imshow(PE1.forward(x),color_continuous_scale="greens")
 
 def single_head_attention(Q: t.Tensor, K: t.Tensor, V: t.Tensor, mask=False) -> t.Tensor:
     '''
-    Should return the results of self-attention 
-    (see the "Self-Attention in Detail" section of the Illustrated Transformer).
-    With this function, you can ignore masking.
-
-    The dimensions of the tensors are batch, 
-    Q: shape =
-    K: shape =
-    V: shape =
-    Return: shape =
+    Return the results of self-attention. Includes masking.
     '''
     d_head = Q.size(-1)
     arg = t.matmul(Q, K.transpose(-2,-1))/np.sqrt(d_head)
     if mask == True:
-        t.masked_fill(arg, t.zeros(arg.shape), -1e18)
+        # t.masked_fill(arg, t.zeros(arg.shape), -1e18)
+        t.masked_fill(arg, t.zeros(arg.shape), -np.inf)
     Z = arg.softmax(dim=-1)
     Z = t.matmul(Z, V)
     return Z
-
-
 
 
 # %% 
@@ -90,3 +81,51 @@ print(single_head_attention(Q, K, V, mask=True))
 
 
 # %%
+
+def multihead_masked_attention(Q: t.tensor, K: t.tensor, V: t.tensor, num_heads: int):
+    """ 
+    Implements multihead masked attention on Q, K, V.
+    Q, K, V shapes: (batch, seq, num_heads*headsize)
+    """
+    batch, seq, head = Q.shape[0], Q.shape[1], Q.shape[2]
+
+    # Reshape Q, K, V into 4D tensors (see exercise description)
+    Q.reshape(batch, seq, num_heads, head)
+    K.reshape(batch, seq, num_heads, head)
+    V.reshape(batch, seq, num_heads, head)
+
+
+    # Calculate attention scores by multiplying Q and K
+
+
+
+    # Apply attention mask
+
+    
+    # Apply softmax to get attention probabilities
+
+
+    # Multiply by matrix V to get attention values
+
+
+    # Rearrange back into a 3D tensor
+
+
+
+
+
+
+
+# %%
+
+# Testing
+
+Q = t.randn((4,4,4))
+print(Q)
+
+Q.reshape(2,2,4,4)
+print(Q)
+
+
+A = t.arange(4)
+print(A)
